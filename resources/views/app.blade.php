67c9ae51881f283b199ac78223dc0e3bf13708c6
Vasu Grover <notcoderguy@gmail.com>
2025-05-06 01:17:40 +0530

    feat: update favicons and meta tags

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,50 +1,72 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" @class(['dark' => ($appearance ?? 'system') == 'dark'])>
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" @class(['dark'=> ($appearance ?? 'system') == 'dark'])>
 
-        {{-- Inline script to detect system dark mode preference and apply it immediately --}}
-        <script>
-            (function() {
-                const appearance = '{{ $appearance ?? "system" }}';
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-                if (appearance === 'system') {
-                    const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+    {{-- Inline script to detect system dark mode preference and apply it immediately --}}
+    <script>
+        (function() {
+            const appearance = '{{ $appearance ?? "system" }}';
 
-                    if (prefersDark) {
-                        document.documentElement.classList.add('dark');
-                    }
+            if (appearance === 'system') {
+                const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+                if (prefersDark) {
+                    document.documentElement.classList.add('dark');
                 }
-            })();
-        </script>
-
-        {{-- Inline style to set the HTML background color based on our theme in app.css --}}
-        <style>
-            html {
-                background-color: oklch(1 0 0);
             }
+        })();
+    </script>
 
-            html.dark {
-                background-color: oklch(0.145 0 0);
-            }
-        </style>
+    {{-- Inline style to set the HTML background color based on our theme in app.css --}}
+    <style>
+        html {
+            background-color: oklch(1 0 0);
+        }
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+        html.dark {
+            background-color: oklch(0.145 0 0);
+        }
+    </style>
 
-        <link rel="icon" href="/favicon.ico" sizes="any">
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml">
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+    <title inertia>{{ config('app.name', 'GeoProxy') }}</title>
 
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
-        @routes
-        @viteReactRefresh
-        @vite(['resources/js/app.tsx', "resources/js/pages/{$page['component']}.tsx"])
-        @inertiaHead
-    </head>
-    <body class="font-sans antialiased">
-        @inertia
-    </body>
+    <link rel="icon" type="image/svg+xml" href="/logo.svg">
+    <meta name="msapplication-TileImage" content="/logo.svg">
+
+    <meta name="title" content="GeoProxy">
+    <meta name="description"
+        content="🌐 GeoProxy: Level up your web scraping with Geoproxy's proxy API! 💻 HTTP, SOCKS4, SOCKS5 data at your fingertips. 😉 Stay anonymous, bypass restrictions." />
+
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="https://geoproxy.in/">
+    <meta property="og:title" content="GeoProxy">
+    <meta property="og:description"
+        content="🌐 GeoProxy: Level up your web scraping with Geoproxy's proxy API! 💻 HTTP, SOCKS4, SOCKS5 data at your fingertips. 😉 Stay anonymous, bypass restrictions." />
+    <meta property="og:image" content="/banner.png">
+
+    <!-- Twitter -->
+    <meta property="twitter:card" content="summary_large_image">
+    <meta property="twitter:url" content="https://geoproxy.in/">
+    <meta property="twitter:title" content="GeoProxy">
+    <meta property="twitter:description"
+        content="🌐 GeoProxy: Level up your web scraping with Geoproxy's proxy API! 💻 HTTP, SOCKS4, SOCKS5 data at your fingertips. 😉 Stay anonymous, bypass restrictions.">
+    <meta property="twitter:image" content="/banner.png">
+
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+
+    @routes
+    @viteReactRefresh
+    @vite(['resources/js/app.tsx', "resources/js/pages/{$page['component']}.tsx"])
+    @inertiaHead
+</head>
+
+<body class="font-sans antialiased">
+    @inertia
+</body>
+
 </html>
